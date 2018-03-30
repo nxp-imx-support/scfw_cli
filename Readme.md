2992235b3267fde2493645078e5a61b83df382a7
Manuel Rodriguez <manuel.rodriguez@nxp.com>
2018-03-30 16:32:22 -0400

    Add support to get/set clock rate

--- Readme.md
+++ Readme.md
@@ -7,7 +7,9 @@ A command line interface that allows you to make SCFW requests using QNX's conso
 In order to build this on your system you will need to modify the Makefile:
 You will need to build the BSP for the board you want to use and modify the BSP_DIR
 variable in the Makefile to point to your BSP directory. 
+
 e.g:
+
 BSP_DIR= /home/Your/BSP/root/dir
 
 The BSP contains the headers/libraries that this project needs to link against.
@@ -30,14 +32,16 @@ selected but no parameter the user will be prompted to enter the parameters.
 		Power Management Service =pm
 	-opt: The functions supported are:
 		sc_pm_set_resource_power_mode =set-rsrc-pw
-		sc_pm_get_resource_power_mode =get_rsrc-pw
+		sc_pm_get_resource_power_mode =get-rsrc-pw
+		sc_pm_set_clock_rate =set-clk-rate
+		sc_pm_get_clock_rate =get-clk-rate
 	-param: The parameters will vary depending on the function that will be called, but all parameters are delimited by '_'.
 
 	Parameters used by each function:
 		Power management service:
 			set-rsrc-pw: This functions takes 3 parameters, firstResource_lastResource_powerMode
-					The firstResource parameter is the first resource that will change it's powerMode.
-					The lastResource parameter is the last resource that will change it's powerMode.
+					The firstResource parameter is the first resource that will change its powerMode.
+					The lastResource parameter is the last resource that will change its powerMode.
 					The powerMode parameter is the power mode to set for the parameters, the options are:
 							OFF = 0
 							STANDBY = 1
@@ -56,6 +60,35 @@ selected but no parameter the user will be prompted to enter the parameters.
 					    scfw_cli -svc=pm -opt=get-rsrc-pw -param=0_541
 					To get the power mode from resource 0 call: 
 					    scfw_cli -svc=pm -opt=get-rsrc-pw -param=0_0
+			set-rsrc-pw: This functions takes 3 parameters, resource_clock_rate
+					The resource parameter is the resource that will change its clock rate.
+					The clock parameter is the clock type that will change its clock rate, the options are:
+						SC_PM_CLK_SLV_BUS       0
+						SC_PM_CLK_MST_BUS       1
+						SC_PM_CLK_PER           2
+						SC_PM_CLK_PHY           3
+						SC_PM_CLK_MISC          4
+						SC_PM_CLK_MISC0         0
+						SC_PM_CLK_MISC1         1
+						SC_PM_CLK_MISC2         2
+						SC_PM_CLK_MISC3         3
+						SC_PM_CLK_MISC4         4
+						SC_PM_CLK_CPU           2
+						SC_PM_CLK_PLL           4
+						SC_PM_CLK_BYPASS        4
+					Refer to the SCFW documentation 'Clock list' for the type of clock that each resource has.
+					The rate parameter is the clock rate to set in Hz.
+					To set the CPU clock rate to 1200MHz on SC_R_A53 call: 
+					    scfw_cli -svc=pm -opt=set-clk-rate -param=0_2_1200000000
+					where 0 is the resource number, 2 is SC_PM_CLK_CPU and 1200000000 is the frequency in Hz.
+			get-clk-rate: This function takes 2 parameters, firstResource_lastResource
+					The firsResource parameter is the first resource to get its clock rates from.
+					The lastResource parameter is the last resource to get its clock rates from.
+					If only one resource is needed then set firstResource and lastResource equal.
+					To get the clock rates from all resources call: 
+					    scfw_cli -svc=pm -opt=get-clk-rate -param=0_541
+					To get the clock rates from resource 0 call: 
+					    scfw_cli -svc=pm -opt=get-clk-rate -param=0_0
 
 ## Authors
 
--- src/pm/pm.c
+++ src/pm/pm.c
@@ -27,12 +27,19 @@ static const char * sc_power_mode_str[] = {
 
 typedef enum pm_opt_e{
 	get_pw = 1,						/* Get Resource Power Mode option*/
-	set_pw,						/* Set Resource Power Mode option */
+	set_pw,							/* Set Resource Power Mode option */
+	get_clk_rate,					/* Get clock rate */
+	set_clk_rate,					/* Set clock rate */
 	inv
 }pm_opt_t;
 
+/* Defines */
+#define LAST_CLK	4
+
 static void pm_get_power_mode(int fd, char *param);
 static void pm_set_power_mode(int fd, char *param);
+static void pm_get_clk_rate(int fd, char *param);
+static void pm_set_clk_rate(int fd, char *param);
 
 /* Helper functions */
 static const char* sc_pw2str(sc_pm_power_mode_t pw);
@@ -48,30 +55,29 @@ void pm_service_main(int fd, char *argv[]){
 		printf("Select option: \n");
 		printf("1.- Get resources power mode \n");
 		printf("2.- Set resource power mode \n");
+		printf("3.- Get clock rate\n");
+		printf("4.- Set clock rate\n");
 
 		scanf("%d", &svc);
-		switch(svc){
-			case get_pw:
-				pm_opt = get_pw;
-				break;
-			case set_pw:
-				pm_opt = set_pw;
-				break;
-			default:
-				printf("Please select a valid option\n");
-				break;
+		if((svc >= inv) | (svc < get_pw)){
+			pm_opt = inv;
+			printf("Please select a valid option\n");
+		} else{
+			pm_opt = svc;
 		}
 	} else {
 		/* Check if a valid service option was passed */
-		if (argv[0] != NULL){
-			if(strncmp(argv[0], "get-rsrc-pw", strlen("get-rsrc-pw")) == 0){
-				pm_opt = get_pw;
-			} else if(strncmp(argv[0], "set-rsrc-pw", strlen("set-rsrc-pw")) == 0){
-				pm_opt = set_pw;
-			} else{
-				pm_opt = inv;
-				printf("PM: %s is an invalid service option\n", argv[0]);
-			}
+		if(strncmp(argv[0], "get-rsrc-pw", strlen("get-rsrc-pw")) == 0){
+			pm_opt = get_pw;
+		} else if(strncmp(argv[0], "set-rsrc-pw", strlen("set-rsrc-pw")) == 0){
+			pm_opt = set_pw;
+		} else if(strncmp(argv[0], "get-clk-rate", strlen("get-clk-rate")) == 0){
+			pm_opt = get_clk_rate;
+		} else if(strncmp(argv[0], "set-clk-rate", strlen("set-clk-rate")) == 0){
+			pm_opt = set_clk_rate;
+		} else{
+			pm_opt = inv;
+			printf("PM: %s is an invalid service option\n", argv[0]);
 		}
 	}
 
@@ -82,6 +88,12 @@ void pm_service_main(int fd, char *argv[]){
 				break;
 			case set_pw:
 				pm_set_power_mode(fd, argv[1]);
+				break;
+			case get_clk_rate:
+				pm_get_clk_rate(fd, argv[1]);
+				break;
+			case set_clk_rate:
+				pm_set_clk_rate(fd, argv[1]);
 				break;
 			default:
 				printf("PM: invalid service option\n");
@@ -153,7 +165,7 @@ void pm_get_power_mode(int fd, char *param){
 void pm_set_power_mode(int fd, char *param){
 
 	int r_first = 0, r_last = 0;
-	sc_pm_power_mode_t power_mode = SC_PM_PW_MODE_ON;			/* On is usually less harmless that turning things off*/
+	sc_pm_power_mode_t power_mode = SC_PM_PW_MODE_ON;			/* On is usually less harmful that turning things off*/
 
 	/* Process arguments */
 	if(param == NULL){
@@ -228,6 +240,154 @@ void pm_set_power_mode(int fd, char *param){
 	}
 }
 
+
+void pm_get_clk_rate(int fd, char *param){
+
+	int r_first = 0, r_last = 0;
+
+	/* Process arguments */
+	if(param == NULL){
+		int ret, valid = 0;
+		do{
+			printf("Enter resource numbers in the following format: \n");
+			printf("r_first r_last For instance 1 4 to get resources 1 through 4\n");
+			printf("if only one resource is required call rsrc rsrc e.g. 1 1 for resource no 1\n");
+			printf("or enter 0 %d for all resources\n", SC_R_LAST);
+			ret = scanf("%d %d", &r_first, &r_last);
+			if(ret == 2){
+				valid = 1;
+			} else{
+				printf("Invalid option\n");
+				valid = 0;
+			}
+		} while(valid == 0);
+	} else{
+		/* Parse parameters */
+		int tmp[2];
+		int err;
+		err = parse_param(param, tmp, 2);
+		if(err == 2){
+			r_first = tmp[0];
+			r_last = tmp[1];
+		} else {
+			printf("PM:SET_CLK_RATE:Invalid -param=\n");
+			return;
+		}
+	}
+
+	/* Keep resources within limits */
+	if(r_last >= SC_R_LAST){
+		r_last = SC_R_LAST - 1;
+	}
+	if(r_first >= SC_R_LAST){
+		r_first = SC_R_LAST - 1;
+	}
+
+	/* Get resources clock rate */
+	printf("Resource, Clock, Clock rate\n");
+	imx_dcmd_sc_pm_clock_rate_t clk_rate;
+	sc_err_t status;
+	int err, cnt = 0;
+	for(; r_first <= r_last; r_first++){
+		clk_rate.resource = r_first;
+
+		for(clk_rate.clk = 0; clk_rate.clk <= LAST_CLK; clk_rate.clk++){
+			/* Get resource clock rate for all clocks */
+			do {
+				err = devctl(fd, IMX_DCMD_SC_PM_GET_CLOCK_RATE, &clk_rate, sizeof(imx_dcmd_sc_pm_clock_rate_t), (int *) &status);
+			} while ((err == EAGAIN) && (cnt++ < 10) && (delay(10) == 0));
+			if(status == SC_ERR_NONE){
+				printf("%s,clock:%d, rate:%d\n", sc_rsrc2str(r_first), clk_rate.clk, clk_rate.rate);
+			} else {
+				printf("ERR:%s,%s\n", sc_rsrc2str(r_first), sc_status2str(status));
+			}
+		}
+	}
+}
+
+void pm_set_clk_rate(int fd, char *param){
+
+	imx_dcmd_sc_pm_clock_rate_t clk_rate;
+
+	/* Process arguments */
+	if(param == NULL){
+		int ret, valid = 0;
+		do {
+			printf("Enter resource number: \n");
+			ret = scanf("%d", &clk_rate.resource);
+			if(ret == 1){
+				/* Keep resources within limits */
+				int tmp = clk_rate.resource; /* Workaround for scanf bug */
+				printf("Enter clock type to be set, options:\n");
+				printf("0 - SC_PM_CLK_SLV_BUS\n");
+				printf("1 - SC_PM_CLK_MST_BUS\n");
+				printf("2 - SC_PM_CLK_PER\n");
+				printf("3 - SC_PM_CLK_PHY\n");
+				printf("4 - SC_PM_CLK_MISC\n");
+				printf("0 - SC_PM_CLK_MISC0\n");
+				printf("1 - SC_PM_CLK_MISC1\n");
+				printf("2 - SC_PM_CLK_MISC2\n");
+				printf("3 - SC_PM_CLK_MISC3\n");
+				printf("4 - SC_PM_CLK_MISC4\n");
+				printf("2 - SC_PM_CLK_CPU\n");
+				printf("4 - SC_PM_CLK_PLL\n");
+				printf("4 - SC_PM_CLK_BYPASS\n");
+				scanf("%d", &clk_rate.clk);
+
+				printf("Enter clock rate in Hz:\n");
+				scanf("%u", &clk_rate.rate);
+
+				clk_rate.resource = tmp; /* Workaround for bug where scanf overwrites resource while reading clock rate*/
+				printf("Setting resource %s clock %d  to: %u\n",sc_rsrc2str(clk_rate.resource), clk_rate.clk, clk_rate.rate);
+				valid = 1;
+			} else{
+				printf("Invalid option\n");
+				valid = 0;
+			}
+		} while (valid == 0);
+	} else{
+		/* Parse parameters */
+		int tmp[3];
+		int err;
+		err = parse_param(param, tmp, 3);
+		if(err == 3){
+			clk_rate.resource = tmp[0];
+			clk_rate.clk = tmp[1];
+			clk_rate.rate = tmp[2];
+		} else {
+			printf("PM:SET_CLK_RATE:Invalid -param=\n");
+			return;
+		}
+	}
+
+	/* Check parameters */
+	if(clk_rate.resource > SC_R_LAST){
+		printf("Invalid resource\n");
+		return;
+	}
+	if(clk_rate.clk > LAST_CLK){
+		printf("Invalid clock type\n");
+		return;
+	}
+
+	/* Set resources power mode */
+	sc_err_t status;
+	int err, cnt = 0;
+
+	/* Set resource clock rate */
+	do {
+		err = devctl(fd, IMX_DCMD_SC_PM_SET_CLOCK_RATE, &clk_rate, sizeof(imx_dcmd_sc_pm_clock_rate_t), (int *) &status);
+	} while ((err == EAGAIN) && (cnt++ < 10) && (delay(10) == 0));
+	if(status == SC_ERR_NONE){
+		printf("%s clock %d rate set to: %u\n", sc_rsrc2str(clk_rate.resource), clk_rate.clk, clk_rate.rate);
+	} else {
+		printf("Failed to set %s clock %d rate to: %u, err: %s (%d)\n", sc_rsrc2str(clk_rate.resource), clk_rate.clk, clk_rate.rate, sc_status2str(status), status);
+	}
+}
+
+/* Returns a string from a power mode type
+ *
+ * */
 static const char* sc_pw2str(sc_pm_power_mode_t pw){
 	if(pw <= SC_PM_PW_MODE_ON){
 		return sc_power_mode_str[pw];
