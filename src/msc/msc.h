5b68bb860ba132bddddc9bbe76897ffe12c35b5c
Manuel Rodriguez <manuel.rodriguez@nxp.com>
2018-08-02 10:55:53 -0400

    Update copyright

--- src/msc/msc.h
+++ src/msc/msc.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 NXP
+ * Copyright 2018 NXP
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
  * and associated documentation files (the "Software"), to deal in the Software without restriction,
